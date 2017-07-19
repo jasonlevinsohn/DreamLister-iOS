5529eb7ea386a2b1f9524e7e1356125135475021
Jason Levinsohn <jason.levinsohn@gmail.com>
2017-07-19 16:57:28 -0400

    got the adding items working

--- DreamLister/ItemDetailsVC.swift
+++ DreamLister/ItemDetailsVC.swift
@@ -7,16 +7,113 @@
 //
 
 import UIKit
+import CoreData
 
-class ItemDetailsVC: UIViewController {
-
+class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet weak var titleTxt: CustomTextField!
+    @IBOutlet weak var priceTxt: CustomTextField!
+    @IBOutlet weak var detailsTxt: CustomTextField!
+    
+    var stores = [Store]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let topItem = self.navigationController?.navigationBar.topItem {
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
         }
-    }
+        
+        storePicker.delegate = self
+        storePicker.dataSource = self
+        
+//        let store1 = Store(context: context)
+//        store1.name = "Tractor Supply"
+//        let store2 = Store(context: context)
+//        store2.name = "Amazon"
+//        let store3 = Store(context: context)
+//        store3.name = "Egg Washing Company"
+//        let store4 = Store(context: context)
+//        store4.name = "Best Buy"
+//        let store5 = Store(context: context)
+//        store5.name = "K Mart"
+//        let store6 = Store(context: context)
+//        store6.name = "Blockbuster"
+//        
+//        ad.saveContext()
+        getStores()
 
+
+
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
+    func getStores() {
+        let fetchRequest: NSFetchRequest<Store> = Store.fetchRequest()
+        
+        do {
+            self.stores = try context.fetch(fetchRequest)
+            self.storePicker.reloadAllComponents()
+        } catch {
+            // Handle the error
+        }
+    }
+    @IBAction func saveItem(_ sender: UIButton) {
+        
+        
+        let item = Item(context: context)
+        
+        if let title = titleTxt.text {
+            item.title = title
+        }
+        
+        if let price = priceTxt.text {
+            item.price = (price as NSString).doubleValue
+        }
+        
+        if let details = detailsTxt.text {
+            item.details = details
+        }
+        
+        item.toStore = stores[storePicker.selectedRow(inComponent: 0)]
+        
+        ad.saveContext()
+        
+        navigationController?.popViewController(animated: true)
+        
+        
+        
+        
+        
+    }
+    
+    func loadItemData() {
+        
+    }
+    
+    
+    
+    
+    
+    
    
 }
